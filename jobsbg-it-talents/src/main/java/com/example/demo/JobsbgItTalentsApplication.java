c7ac008002b5961a6c45d0058616eb60a948f824
nikolay <nikolaygalchev@gmail.com>
2019-02-28 12:50:08 +0200

    testing the repo

--- jobsbg-it-talents/src/main/java/com/example/demo/JobsbgItTalentsApplication.java
+++ jobsbg-it-talents/src/main/java/com/example/demo/JobsbgItTalentsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobsbgItTalentsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("TEST");
 		SpringApplication.run(JobsbgItTalentsApplication.class, args);
 	}
 
