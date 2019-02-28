57274533a3fad6c40a1c86455ede29927dc747c5
nikolay <nikolaygalchev@gmail.com>
2019-02-28 12:48:30 +0200

    init commit1

---
+++ jobsbg-it-talents/src/main/java/com/example/demo/JobsbgItTalentsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsbgItTalentsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("TEST");
+		SpringApplication.run(JobsbgItTalentsApplication.class, args);
+	}
+
+}
